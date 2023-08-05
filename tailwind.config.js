797d8eec5832b7c3f0b60be068dc643315459074
hessou benjamin <benjaminnonvignonh@gmail.com>
2023-08-05 11:51:32 +0000

    todo

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      'poppins': ['Poppins', 'sans-serif'],
+      'noto' : ['Noto Sans Vithkuqi', 'sans-serif'],
+      'josefin' : ['Josefin Sans', 'sans-serif']
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
